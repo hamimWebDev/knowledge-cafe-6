947a30bfd67966b2168a9c8de82f11b9a8d91959
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-26 23:35:34 +0600

    Use React Font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import "@fontsource/exo-2";
+import "@fontsource/exo-2/400.css";
+import "@fontsource/exo-2/500.css";
+import "@fontsource/exo-2/600.css";
 import React from "react";
 import Header from "./components/Header/Header";
+import Blogs from "./components/Blogs/Blogs";
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <Blogs></Blogs>
     </div>
   );
 };
