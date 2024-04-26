059e1fad5caf024b0a1ce3eb3a0d2be2d2dabf1b
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-26 23:08:34 +0600

    Create blogs json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import Header from "./components/Header/Header";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      
-    </>
+    <div>
+      <Header></Header>
+    </div>
   );
-}
+};
 
 export default App;
